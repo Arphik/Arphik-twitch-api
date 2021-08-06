5164160a4ce0412d9eec9dd685a5bf132121986e
Arphik <sebo44@gmail.com>
2021-08-06 19:49:48 +0200

    Changes

---
+++ app/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const https = require('https');
+const fs = require('fs');
+
+// use the express-static middleware
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.send('Server works');
+})
+
+app.post('/subcallback', (req, res) => {
+    console.log("response EventsSubCallback");
+    res.send('Subcallback worked!');
+    res.status(200).end();
+});
+
+
+// https.createServer({
+//     key: fs.readFileSync('server.key'),
+//     cert: fs.readFileSync('server.cert')
+//   }, app)
+//   .listen(443, function () {
+//     console.log(`Example app listening on port 443! Go to https://localhost:443/`);
+//   });
+
+app.listen(3000, () => {
+  console.log('Server is running!');
+});
