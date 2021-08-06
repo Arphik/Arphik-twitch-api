da233c060c432986c6c900900625fd5d8d2ac143
Arphik <sebo44@gmail.com>
2021-08-06 08:42:37 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const https = require('https');
+const fs = require('fs');
+
+app.post('/EventsSubCallback', (req, res) => {
+    console.log("response EventsSubCallback");
+    res.status(200).end();
+});
+
+
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+  }, app)
+  .listen(443, function () {
+    console.log(`Example app listening on port 443! Go to https://localhost:443/`);
+  });
